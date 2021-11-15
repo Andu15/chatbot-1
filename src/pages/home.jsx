65f967f3ab8b800b54029e0699b27ff509d916e8
Andu15 <estefania_8_3@hotmail.com>
2021-11-15 17:31:48 -0500

    structure

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <section>
+      <div>
+        <img src='../assets/tottusLogo.png' />
+      </div>
+    </section>
+  )
+}
+
+export default Home;
