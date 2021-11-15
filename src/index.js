9818bda2e4227161d98169645afcdbe912853c1e
Andu15 <estefania_8_3@hotmail.com>
2021-11-15 17:36:29 -0500

    installing ant

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css'
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import { Layout, Row, Col } from 'antd';
+
+const Home = () => {
+  return (
+    <section>
+      <div>
+        <img src='../assets/tottusLogo.png' />
+      </div>
+    </section>
+  )
+}
+
+export default Home;
