3904316c0d96887d57bb82fbb3d9c12f1049b360
Andu15 <estefania_8_3@hotmail.com>
2021-11-16 15:15:44 -0500

    feat: add changes

--- src/pages/LocationProduct.jsx
+++ src/pages/LocationProduct.jsx
@@ -1,42 +1,22 @@
-import React from 'react'
-import chocolate from '../img/chocolate.jpg'
-import galleta from '../img/galleta.jpg'
-import paneton from '../img/paneton.jpg'
-import ubicacion from '../img/ubicacion.png'
+import { useLocation } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
-function LocationProduct() {
+const LocationProduct = () => {
+
+  let {search} = useLocation();
+  let query = new URLSearchParams(search);
+  const tokenPar = query.getAll("token")[0];
+  const decoded = jwt_decode(tokenPar);
+  console.log(decoded)
+  const pasillo = decoded.codigopasillo;
+  console.log(pasillo);
+  // const url = `https://storage.googleapis.\com/tot-bi-corp-chatbot-dev.appspot.com/EXPERIENCIA-DIGITAL/${decoded.codigopais}/LABORATORIA/${decoded.codigotienda}/${decoded.codigojerarquia}-${decoded.codigopasillo}.jpg`;
+
   return (
-    <section className='contenedorTotal d-flex flex-column'>
-      <div className='d-flex flex-row'>
-      <button  type="button" ><i class="fas fa-chevron-left"></i> </button>
-      <p>¿Quieres consultar otro producto?</p>
-      </div>
-      <div>
-        <img src={ubicacion} alt='' className='plano'/>
-      </div>
-      <div id="carouselExampleControlsNoTouching" className="carousel slide contentCarrusel" data-bs-touch="false" data-bs-interval="false" >
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src={chocolate} class="d-block w-100" alt="..." className='cardCarousel' />
-          </div>
-          <div class="carousel-item">
-            <img src={galleta} class="d-block w-100" alt="..." className='cardCarousel' />
-          </div>
-          <div class="carousel-item">
-            <img src={paneton} class="d-block w-100" alt="..." className='cardCarousel' />
-          </div>
-        </div>
-        <button className="carousel-control-prev buttonCar" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next buttonCar" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
+    <section>
+      {/* <img src={url} alt="ubicación del producto"/> */}
     </section>
   )
 }
 
-export default LocationProduct
+export default LocationProduct;
