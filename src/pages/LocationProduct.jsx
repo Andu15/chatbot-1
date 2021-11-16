cdd1299c22cebe6e41a15aaa29ad5d7bfd47859d
RocioSulca <rociosulca.z@gmail.com>
2021-11-16 13:18:17 -0500

    Ubicacion

--- src/pages/LocationProduct.jsx
+++ src/pages/LocationProduct.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import chocolate from '../img/chocolate.jpg'
+import galleta from '../img/galleta.jpg'
+import paneton from '../img/paneton.jpg'
+import ubicacion from '../img/ubicacion.png'
 
-function locationProduct() {
+function LocationProduct() {
   return (
-    <section>
-      Location
+    <section className='contenedorTotal d-flex flex-column'>
+      <div>
+      <button className="" type="button" ></button>
+      </div>
+      <div>
+        <img src={ubicacion} alt='' className='plano'/>
+      </div>
+      <div id="carouselExampleControlsNoTouching" className="carousel slide contentCarrusel" data-bs-touch="false" data-bs-interval="false" >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={chocolate} class="d-block w-100" alt="..." className='cardCarousel' />
+          </div>
+          <div class="carousel-item">
+            <img src={galleta} class="d-block w-100" alt="..." className='cardCarousel' />
+          </div>
+          <div class="carousel-item">
+            <img src={paneton} class="d-block w-100" alt="..." className='cardCarousel' />
+          </div>
+        </div>
+        <button className="carousel-control-prev buttonCar" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next buttonCar" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     </section>
   )
 }
 
-export default locationProduct
+export default LocationProduct
