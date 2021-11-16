08ede6abf05a5623548246c045ea3d70397c14eb
Andu15 <estefania_8_3@hotmail.com>
2021-11-15 23:15:49 -0500

    feat: add templates

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,29 @@
-
+import logo from '../assets/tottusLogo.png';
 
 const Products = () => {
   return (
     <section>
-      Product
+      <section>
+        <div class="row">
+          <div class="col">
+            <img src={logo}/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <h1>Explora</h1>
+            <hr />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <p>Tottus Angamos</p>
+          </div>
+        </div>
+      </section>
+
+      <section></section>
+
     </section>
   )
 }
