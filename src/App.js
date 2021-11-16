0c9d21717908facfbdec87640425ab14412a083f
Andu15 <estefania_8_3@hotmail.com>
2021-11-15 22:33:11 -0500

    fix sass

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import './Sass/app.scss';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Product from './pages/Products.jsx';
+import LocationProduct from './pages/LocationProduct.jsx';
+import StockProduct from './pages/StockProduct.jsx';
 
 const App = () => {
 
