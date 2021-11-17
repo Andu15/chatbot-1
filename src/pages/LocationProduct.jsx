439764ce1125c339888b7f16499910ec6689ed2a
RocioSulca <rociosulca.z@gmail.com>
2021-11-16 22:00:13 -0500

    Carousel

--- src/pages/LocationProduct.jsx
+++ src/pages/LocationProduct.jsx
@@ -1,12 +1,12 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 
-const LocationProduct = ({apiGetCategori, apiGetProduct}) => {
+const LocationProduct = ({ apiGetProduct }) => {
 
-  // console.log(apiGetCategori, apiGetProduct)
-  // const {name} = apiGetCategori;
+  const [dataProducts, setDataProducts] = useState([]);
 
-  let {search} = useLocation();
+  let { search } = useLocation();
   let query = new URLSearchParams(search);
   const tokenPar = query.getAll("token")[0];
   const decoded = jwt_decode(tokenPar);
@@ -15,6 +15,18 @@ const LocationProduct = ({apiGetCategori, apiGetProduct}) => {
 
   const url = `https://storage.googleapis.com/tot-bi-corp-chatbot-dev.appspot.com/EXPERIENCIA-DIGITAL/${decoded.codigopais}/LABORATORIA/${decoded.codigotienda}/${decoded.codigojerarquia}-${pasilloProd}.jpg`;
 
+  const getProduct = async () => {
+    const data = await apiGetProduct(decoded.nombreproducto, '105', '1', '3');
+    setDataProducts(data)
+    console.log(data)
+  }
+
+  useEffect(() => {
+    getProduct();
+
+  }, []);
+
+  console.log(dataProducts)
   return (
     <section className='d-flex flex-column'>
       <div className="containerBack">
@@ -25,24 +37,28 @@ const LocationProduct = ({apiGetCategori, apiGetProduct}) => {
         <p>{decoded.codigocategoria}</p>
       </div>
       <div className="containerImage">
-        <img src={url} alt=''/>
+        <img src={url} alt='' />
       </div>
       <div className="col titleCarousel">
-          <h1>Productos en este pasillo</h1>
-          <hr />
-        </div>
+        <h1>Productos en este pasillo</h1>
+        <hr />
+      </div>
       <div id="carouselExampleControlsNoTouching" className="carousel slide contentCarrusel" data-bs-touch="false" data-bs-interval="false" >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            {/* <img src={chocolate} className="d-block w-100" alt="..." className='cardCarousel' /> */}
-          </div>
-          <div className="carousel-item">
-            {/* <img src={galleta} className="d-block w-100" alt="..." className='cardCarousel' /> */}
-          </div>
-          <div className="carousel-item">
-            {/* <img src={paneton} className="d-block w-100" alt="..." className='cardCarousel' /> */}
-          </div>
+        <div className="carousel-inner" >
+          {
+            dataProducts && dataProducts.map((item, index) => 
+              index === 0 ? (<div className="carousel-item active" key={index}>
+                <img src={item.images} className="d-block w-50" alt={item.name} />
+                <p>{item.description}</p>
+                <p>{item.prices}</p>
+              </div>) : (<div className="carousel-item " key={index}><img src={item.images} className="d-block w-50" alt={item.name} />
+                <p>{item.description}</p>
+                <p>{item.prices}</p>
+              </div>)
+            
+            )}
         </div>
+
         <button className="carousel-control-prev buttonCar" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Previous</span>
