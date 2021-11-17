ced541b45e6f91eb201a122874e239f3b63ee585
makemile <mkengya@gmail.com>
2021-11-17 00:35:10 -0500

    resolviendo conflictos

--- src/pages/StockProduct.jsx
+++ src/pages/StockProduct.jsx
@@ -12,27 +12,9 @@ const StockProduct = ({ apiGetProduct }) => {
   console.log(apiGetProduct);
 
   return (
-<<<<<<< HEAD
-    // style="width: 18rem;"
-    <div class="card">
-  <div><h5 class="card-title">nombre del producto</h5></div>
-  <img src="/" class="card-img-top" alt="product"/>
-  <div class="card-body">
-    
-    <p class="card-text">Descripcion del producto</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
-  </ul>
-  
-</div>
-=======
     <section>
       <BtnReturn />
     </section>
->>>>>>> 7080353d91289e4c7ee7a2d06edbf6137a0de170
   )
 }
 
