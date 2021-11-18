94cd384fd1312fe8ef7d9babf2aabc0ad8a1a965
CatherineChoque <85088235+CatherineChoque@users.noreply.github.com>
2021-11-18 12:34:59 -0500

    details on styles in stock view

--- src/components/StockAvalaible.jsx
+++ src/components/StockAvalaible.jsx
@@ -1,12 +1,17 @@
 
 const StockAvalaible = ({uniqueProduct}) => {
 
+  const leermas = () =>{
+    console.log('aqui va el texto leer mas')
+    //<p>{e.description}</p>
+  }
   const spinner = <div class="spinner-border text-success" role="status">
                     <span class="visually-hidden">Loading...</span>
                   </div>
-
+  
   const succesfulData = uniqueProduct.map(e=>
     (
+
     <section className="viewStockproduct" key={e.codigosku}>
       <section className="containerImageStock">
                               <img src={e.images} alt={e.name}/>
@@ -14,10 +19,9 @@ const StockAvalaible = ({uniqueProduct}) => {
       <section className="containerTextStock">
         <div className="allTextStock">
           <h3>{e.name}</h3>
-          <p>{e.marca}</p>
-          <p>S/ {e.prices}</p>
+          <p>S/ {e.prices} UN</p>
           {
-            e.description.length > 50 ? <p>{e.description.substring(0, 100)}...</p> :
+            e.description.length > 50 ? <p>{e.description.substring(0, 100)}<span onClick={leermas}> leer mas...</span></p> :
             <p>{e.description}</p>
           }
         </div>
