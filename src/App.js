cb86b5ed65bb0ea2a3c9134d2dc23151d1fa0150
Jorge Castro <ext_jocastroh@tottus.com.pe>
2021-11-18 21:20:35 -0500

    fix header

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Products from './pages/Products.jsx';
 import LocationProduct from './pages/LocationProduct.jsx';
 import StockProduct from './pages/StockProduct.jsx';
 import Error404 from './pages/Error404';
-import { apiGetProduct, apiGetProductSku} from './APIS/api';
+import { apiGetProduct, apiGetProductSku } from './APIS/api';
 
 const App = () => {
 
@@ -17,20 +17,17 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/locationproduct" element={<LocationProduct
-            
             apiGetProduct={apiGetProduct}
             apiGetProductSku={apiGetProductSku}
-            />}/>
+          />} />
           <Route path="/stockproduct" element={<StockProduct
-            
             apiGetProduct={apiGetProduct}
             apiGetProductSku={apiGetProductSku}
-            />} />
+          />} />
           <Route path="/experienciatienda" element={<Products
-           
             apiGetProduct={apiGetProduct}
             apiGetProductSku={apiGetProductSku}
-            />} />
+          />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
       </BrowserRouter>
