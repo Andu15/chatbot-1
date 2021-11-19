75cb03e204d1ae21f1912a29bf8c79eead567771
Jorge Castro <ext_jocastroh@tottus.com.pe>
2021-11-18 21:20:35 -0500

    fix header

--- src/APIS/api.js
+++ src/APIS/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { datapasilloCategory } from "./category";
-import {dataTienda}  from "./tiendasInfo";
+import { dataTienda } from "./tiendasInfo";
 
 
 console.log("estas en la nueva repo");
@@ -8,28 +8,30 @@ console.log("estas en la nueva repo");
 
 export const apiGetProduct = async (prod, tienda, pagInicio, pagFinal) => {
   const url = `https://lid-per-dot-tot-bi-corp-chatbot-dev.appspot.com/api-per/product-search?q=${prod}&channel=${tienda}&page=${pagInicio}&perPage=${pagFinal}`;
-  
+
   //const url = `https://www.tottus.com.pe/api/product-search?q=${prod}&channel=${tienda}&page=${pagInicio}&perPage=${pagFinal}`;
-  const dataAxios = await axios({
+  let dataAxios = await axios({
     method: "GET",
-    withCredentials:false,
-    headers: { 
-      'x-country': 'PE', 
-      'x-commerce': 'Tottus', 
-     'x-usrtx': 'tss',
+    withCredentials: false,
+    headers: {
+      'x-country': 'PE',
+      'x-commerce': 'Tottus',
+      'x-usrtx': 'tss',
     },
     url: url,
   });
 
-  const resData = dataAxios.data.results; 
+
+
+  const resData = dataAxios.data.results;
 
   const resultado = resData.map((key) => {
-  
-     //filtrra pasillos 
-     const filterpasillo = datapasilloCategory.filter(
-      (res) =>{
+
+    //filtrra pasillos 
+    const filterpasillo = datapasilloCategory.filter(
+      (res) => {
         return (res.codigo_tienda === Number(tienda) &&
-        res.jerarquia === key.attributes.hierarchy.slice(0,9))
+          res.jerarquia === key.attributes.hierarchy.slice(0, 9))
       }
     );
 
@@ -42,8 +44,8 @@ export const apiGetProduct = async (prod, tienda, pagInicio, pagFinal) => {
       marca: key.attributes.marca,
       ean: key.attributes.ean,
       description: key.description,
-      codigojerarquia:(key.attributes.hierarchy.slice(0,9)),
-      codigopasillo:(filterpasillo.length>0)?((filterpasillo.map( key => key.pasillo))[0]):" ",
+      codigojerarquia: (key.attributes.hierarchy.slice(0, 9)),
+      codigopasillo: (filterpasillo.length > 0) ? ((filterpasillo.map(key => key.pasillo))[0]) : " ",
       codigopais: "PE",
       nombreproducto: key.name, //tengo duda aqui si debe ser categroia o producto
       codigotienda: tienda,
@@ -59,14 +61,14 @@ export const apiGetProductSku = async (sku, tienda) => {
   const url = `https://lid-per-dot-tot-bi-corp-chatbot-dev.appspot.com/api-per/skuList?productsList=${sku}`;
   //const url = `https://www.tottus.com.pe/api/content/skuList?productsList%5B0%5D=${sku}`;
   const dataAxios = await axios({
-    method: "GET",
-    // withCredentials:false,
-    headers: { 
-      'x-country':'PE', 
-      'x-commerce':'Tottus', 
-     'x-usrtx': 'tss',
-    },
     url: url,
+    withCredentials: false,
+    method: "GET",
+    headers: {
+      'x-country': 'PE',
+      'x-commerce': 'Tottus',
+      'x-usrtx': 'tss',
+    }
   });
 
   const resData = dataAxios.data.results;
@@ -75,14 +77,12 @@ export const apiGetProductSku = async (sku, tienda) => {
 
     //filtrra pasillos 
     const filterpasillo = datapasilloCategory.filter(
-      (res) =>{
+      (res) => {
         return (res.codigo_tienda === Number(tienda) &&
-        res.jerarquia=== key.attributes.hierarchy.slice(0,9))
+          res.jerarquia === key.attributes.hierarchy.slice(0, 9))
       }
     );
 
-    console.log("pasillllll",filterpasillo );
-    
     const data = {
       id: key.id,
       images: key.images[0],
@@ -91,10 +91,10 @@ export const apiGetProductSku = async (sku, tienda) => {
       codigosku: key.sku,
       description: key.description,
       ean: key.attributes.ean,
-      codigojerarquia:(key.attributes.hierarchy.slice(0,9)),
-      codigopasillo: (filterpasillo.length>0)?(filterpasillo[0].pasillo):"",
+      codigojerarquia: (key.attributes.hierarchy.slice(0, 9)),
+      codigopasillo: (filterpasillo.length > 0) ? (filterpasillo[0].pasillo) : "",
       codigopais: "PE",
-      nombreproducto:key.name, //tengo duda aqui si debe ser categroia o producto
+      nombreproducto: key.name, //tengo duda aqui si debe ser categroia o producto
       codigotienda: tienda,
     };
     return data;
@@ -106,44 +106,45 @@ export const apiGetProductSku = async (sku, tienda) => {
 
 //traer data de stock de acuerdo a un sku .
 export const getStockSku = async (sku) => {
- 
+
   const url = `https://lid-per-dot-tot-bi-corp-chatbot-dev.appspot.com/api-per/stock?sku=${sku}`;
   let stockAxios = await axios({
-        method: "GET",
-        withCredentials:false,
-        headers: { 
-          'x-country':'PE', 
-          'x-commerce':'Tottus', 
-         'x-usrtx':'tss',
-        },
-        url: url,
- });  
+    method: "GET",
+    withCredentials: false,
+    headers: {
+      'x-country': 'PE',
+      'x-commerce': 'Tottus',
+      'x-usrtx': 'tss',
+
+    },
+    url: url,
+  });
 
 
-  if(stockAxios.data!==0){
+  if (stockAxios.data !== 0) {
     console.log(" hay stock");
 
-    const dataStock = stockAxios.data.sku.map( (stock)=> {
-      let nameStore= dataTienda.filter( str => str.CodigoLocal===stock.store);
-  
-      if (nameStore[0]===undefined){
-        nameStore[0]=" ";
+    const dataStock = stockAxios.data.sku.map((stock) => {
+      let nameStore = dataTienda.filter(str => str.CodigoLocal === stock.store);
+
+      if (nameStore[0] === undefined) {
+        nameStore[0] = " ";
       };
-  
+
       const data = {
-        store:stock.store,
-        storeName:nameStore[0].DescripcionLocal?nameStore[0].DescripcionLocal:" ",
-        storeImagen:nameStore[0].Imagen?nameStore[0].Imagen:"https://static.wikia.nocookie.net/logopedia/images/9/92/Tottus_logo_apilado_2006.svg/revision/latest/scale-to-width-down/250?cb=20210325031327&path-prefix=es",
-        stockAvailable:stock.stockAvailable,
-        stockOnLine:stock.stockOnLine,
+        store: stock.store,
+        storeName: nameStore[0].DescripcionLocal ? nameStore[0].DescripcionLocal : " ",
+        storeImagen: nameStore[0].Imagen ? nameStore[0].Imagen : "https://static.wikia.nocookie.net/logopedia/images/9/92/Tottus_logo_apilado_2006.svg/revision/latest/scale-to-width-down/250?cb=20210325031327&path-prefix=es",
+        stockAvailable: stock.stockAvailable,
+        stockOnLine: stock.stockOnLine,
       };
       return data;
-  
+
     });
-    console.log("datastockapi",dataStock);
-  
-    return  dataStock;
-  }else {
+    console.log("datastockapi", dataStock);
+
+    return dataStock;
+  } else {
     console.log("no hay stock");
     return ([]);
   }
--- src/pages/LocationProduct.jsx
+++ src/pages/LocationProduct.jsx
@@ -3,18 +3,18 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import imgDefault from '../assets/oops.jpg';
-import {getStockSku,apiGetProductSku} from '../APIS/api';
+import { getStockSku, apiGetProduct } from '../APIS/api';
 
-const LocationProduct = ({ apiGetProduct }) => {
+const LocationProduct = () => {
   const [dataProducts, setDataProducts] = useState([]);
-  console.log(dataProducts)
+  // console.log(dataProducts)
 
   let { search } = useLocation();
   let query = new URLSearchParams(search);
   const tokenPar = query.getAll("token")[0];
   const decoded = jwt_decode(tokenPar);
 
-  console.log(decoded);
+  // console.log(decoded);
   sessionStorage.setItem('nombretienda', decoded.nombretienda);
   sessionStorage.setItem('codigotienda', decoded.codigotienda);
   sessionStorage.setItem('codigopais', decoded.codigopais);
@@ -28,10 +28,11 @@ const LocationProduct = ({ apiGetProduct }) => {
   }
 
   const getProduct = async () => {
+  
 
     const data = await apiGetProduct(decoded.nombreproducto, '123', '1', '5');
-    console.log("dataproducts",data );
-    
+    console.log("dataproducts", data);
+
     const orderData = data.sort(function (a, b) {
       if (a.marca === "tottus") {
         return 1;
@@ -41,14 +42,14 @@ const LocationProduct = ({ apiGetProduct }) => {
       return 0;
     });
     setDataProducts(orderData);
-    console.log(orderData)
+    // console.log(orderData)
   };
 
   useEffect(() => {
     getProduct();
 
   }, []);
-  console.log(decoded);
+  // console.log(decoded);
 
   return (
     <section className='d-flex flex-column'>
